c618eb6be5a7e8f3e6625ce45aa424be661ed4e7
rhjoerg <rhjoerg@gmail.com>
2020-05-25 08:40:44 +0200

    Adjusted generated names, icond, etc.

--- src/main/java/ch/rhj/eclipse/tools/handler/SampleHandler.java
+++ src/main/java/ch/rhj/eclipse/tools/handler/SampleHandler.java
@@ -3,19 +3,19 @@ package ch.rhj.eclipse.tools.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 public class SampleHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"RHJ Eclipse Tools",
-				"Hello from RHJ Ecliipse Tools");
+
+		MessageDialog.openInformation(window.getShell(), "RHJ Eclipse Tools", "Hello from RHJ Eclipse Tools");
+
 		return null;
 	}
 }
